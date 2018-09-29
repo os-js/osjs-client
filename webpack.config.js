8ef3d1024a579205b3f93b544fa8d455a96117ee
Anders Evenrud <andersevenrud@gmail.com>
2018-09-29 22:47:30 +0200

    Updated @osjs/common

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,7 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules|bower_components)\/(?!@osjs)/,
         use: {
           loader: 'babel-loader'
         }

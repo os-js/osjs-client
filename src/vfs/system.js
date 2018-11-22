d4e7d35c6b38bac7b7d351ad911c31e28c76f8ba
Anders Evenrud <andersevenrud@gmail.com>
2018-11-22 21:39:08 +0100

    Updated url() in system vfs adapter

--- src/vfs/system.js
+++ src/vfs/system.js
@@ -87,7 +87,7 @@ const adapter = (core) => {
       request('stat', {path, options}, 'json').then(({body}) => body),
 
     url: ({path}, options) =>
-      Promise.resolve(`/vfs/readfile?path=${encodeURIComponent(path)}`),
+      Promise.resolve(core.url(`/vfs/readfile?path=${encodeURIComponent(path)}`)),
 
     search: ({path}, pattern, options) =>
       request('search', {root: path, pattern, options}, 'json')

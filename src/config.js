11408de12a955e3b3b12819db6ef158c2a043e01
Anders Evenrud <andersevenrud@gmail.com>
2018-12-29 23:19:49 +0100

    Updated default path to 'home:/'

--- src/config.js
+++ src/config.js
@@ -182,7 +182,7 @@ export const defaultConfiguration = {
   },
 
   vfs: {
-    defaultPath: 'osjs:/',
+    defaultPath: 'home:/',
     defaultAdapter: 'system',
     adapters: {},
     mountpoints: [{

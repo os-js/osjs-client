6849f1c2b91bd8e69f969602f3921b6401c85527
Anders Evenrud <andersevenrud@gmail.com>
2020-02-25 22:40:49 +0100

    Added default 'omit' settings to Core by default
    This adds 'desktop.settings.panels' to 'omit' by default.
    Should avoid some confusion with double panels when modified.

--- src/core.js
+++ src/core.js
@@ -53,19 +53,24 @@ export default class Core extends CoreBase {
    * @param {String[]} [options.classNames] List of class names to apply to root dom element
    */
   constructor(config = {}, options = {}) {
-    options = Object.assign({}, {
+    const coreOptions = Object.assign({}, {
+      omit: [],
       classNames: ['osjs-root'],
       root: document.body
     }, options || {});
 
-    super(defaultConfiguration, config, options);
+    if (coreOptions.omit.indexOf('desktop.settings.panels') === -1) {
+      coreOptions.omit.push('desktop.settings.panels');
+    }
+
+    super(defaultConfiguration, config, coreOptions);
 
     this.logger = logger;
     this.ws = null;
     this.ping = null;
     this.splash = new Splash(this);
-    this.$root = options.root;
-    this.$resourceRoot = options.resourceRoot || document.querySelector('head');
+    this.$root = coreOptions.root;
+    this.$resourceRoot = coreOptions.resourceRoot || document.querySelector('head');
     this.requestOptions = {};
     this.urlResolver = urlResolver(this.configuration);
     this.user = {

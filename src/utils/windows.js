35875cbe00af25411f606d27b52490d652ed9f88
Anders Evenrud <andersevenrud@gmail.com>
2020-04-29 22:02:54 +0200

    Added window attribute for disabling droppable (#113)

--- src/utils/windows.js
+++ src/utils/windows.js
@@ -56,6 +56,7 @@ export const createAttributes = attrs => ({
   visibility: 'global',
   shadowDOM: false,
   clamp: true,
+  droppable: true,
   mediaQueries: {
     small: 'screen and (max-width: 640px)',
     medium: 'screen and (min-width: 640px) and (max-width: 1024px)',

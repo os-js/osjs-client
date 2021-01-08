a1d0aa55f9e9852a6ee9f57308754f2299a7b7f9
Anders Evenrud <andersevenrud@gmail.com>
2021-01-08 23:59:41 +0100

    Export configuration and logo

--- index.js
+++ index.js
@@ -55,3 +55,5 @@ export {default as Packages} from './src/packages';
 export {default as Filesystem} from './src/filesystem';
 export {default as Clipboard} from './src/clipboard';
 export {BasicApplication} from './src/basic-application';
+export {defaultConfiguration as configuration} from './src/config';
+export {default as icon} from './src/styles/logo-blue-32x32.png';

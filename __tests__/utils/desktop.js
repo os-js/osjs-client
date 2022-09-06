93eec62a121952b0f7f75ef7530d3eb3d3cebc31
Aaron Meese <ajmeese7@gmail.com>
2022-09-06 22:35:36 +0000

    Added additional tests

--- __tests__/utils/desktop.js
+++ __tests__/utils/desktop.js
@@ -2,19 +2,43 @@ import {createInstance} from 'osjs';
 import {resourceResolver} from '../../src/utils/desktop';
 
 describe('#resourceResolver icon themes', () => {
-  let core;
+  let resolver;
 
-  beforeAll(() => createInstance().then((c) => (core = c)));
+  beforeAll(() => createInstance().then(
+    (core) => (resolver = resourceResolver(core))
+  ));
   afterAll(() => core.destroy());
 
   test('Should work with image extensions in the icon name', () => {
-    const resolver = resourceResolver(core);
-
     expect(resolver.icon('test.png')).toBe(
       '/icons/GnomeIcons/icons/test.png'
     );
     expect(resolver.icon('test.svg')).toBe(
       '/icons/GnomeIcons/icons/test.png'
+    );
+  });
+
+  test('Should work without image extensions in the icon name', () => {
+    expect(resolver.icon('test')).toBe(
+      '/icons/GnomeIcons/icons/test.png'
+    );
+  });
+
+  test('Should work with multiple image extensions in the name', () => {
+    expect(resolver.icon('test.png.svg.gif')).toBe(
+      '/icons/GnomeIcons/icons/test.png.svg.png'
+    );
+  });
+
+  test('Should work on filenames with dots in them', () => {
+    expect(resolver.icon('test.icon.gif')).toBe(
+      '/icons/GnomeIcons/icons/test.icon.png'
+    );
+    expect(resolver.icon('test.icon')).toBe(
+      '/icons/GnomeIcons/icons/test.icon.png'
+    );
+    expect(resolver.icon('lots.of.dots.here')).toBe(
+      '/icons/GnomeIcons/icons/lots.of.dots.here.png'
     );
   });
 });
